32d2201b1dd38b41703694346593648cea1e3a31
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-05-15 15:28:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert("Hello! My name is Caryn!!");
+}
+
+console.log('Hello World');
+2+2
+alert('This is the browsers alert box')
+
+
+let name='cj-the-owl';
+console.log(name);
+
+var interestRate='0.3';
+var interestRate='1';
+console.log(interestRate);

2498e3f9c13d1721ef1875381214e76334552cc0
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-05-16 10:53:32 +0200

    second commit

--- index.js
+++ index.js
@@ -7,9 +7,41 @@ console.log('Hello World');
 alert('This is the browsers alert box')
 
 
-let name='cj-the-owl';
-console.log(name);
+//let name='cj-the-owl';
+//console.log(name);
 
 var interestRate='0.3';
 var interestRate='1';
 console.log(interestRate);
+
+//const person = {name:'Nadine', age: 19, id:0112125929187};
+//let {name, age, id} = person;
+//console.log(name);
+//console.log(age);
+//console.log(id);
+
+let a,b,c; a=b=c=0;
+c--;
+document.write("The value of a is:", a+"<br/>");
+document.write("The value of b is:", b + "<br/>");
+document.write("The value of c is:", c + "<br/>" );
+
+let str = "Hello There";
+let str2 = 'Single quotes works fine';
+let phrase = 'can embed $(str)';
+console.log(str +  str2);
+
+//checking typeof
+//let name = 'John'
+//let age = 30
+let isUser = true
+let firstName = undefined
+//let selectedColor = null
+
+//Objects
+let person = {name: 'John', age: 30};
+console.log(person);
+
+//Arrays
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
